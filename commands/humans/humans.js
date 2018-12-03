4e65a51a32a20853831627ef03a22e6900ef55f4
Flynn <flynnamos@hotmail.com>
2018-12-03 16:01:08 +0000

    put it in a file if it cant send

--- commands/humans/humans.js
+++ commands/humans/humans.js
@@ -52,6 +52,10 @@ module.exports = class ReplyCommand extends Command {
             output += "human ID " + s.countries[owner].humans[h].id + (s.countries[owner].humans[h].building ? " is working building ID " + s.countries[owner].humans[h].building : " isn't working any building") + " and is " + s.countries[owner].humans[h].age + " tick" + (s.countries[owner].humans[h].age == 1 ? "" : "s") + " old.\n";
         }
         output += "```";
+        if (output.length > 1995) {
+			fs.writeFileSync("all.txt", output);
+			return msg.say("Um... ok.", {file: "all.txt"});
+        }
         return msg.say(output);
     }
 };
